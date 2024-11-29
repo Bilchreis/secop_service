54a2db5ff307174152862d87ff22820fc4d74a99
Peter Braun <peter.braun@helmholtz-berlin.de>
2024-11-29 16:55:56 +0100

    initial commit

---
+++ lib/secop_service/mailer.ex
@@ -0,0 +1,3 @@
+defmodule SecopService.Mailer do
+  use Swoosh.Mailer, otp_app: :secop_service
+end
---
+++ lib/secop_service_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule SecopServiceWeb.PageController do
+  use SecopServiceWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
