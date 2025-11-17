095bf69266ee5e6918c886a4ea7e2988f1c54914
Peter Braun <peter.braun@helmholtz-berlin.de>
2025-11-17 08:56:23 +0100

    esbuild fix for colocated hooks

--- config/config.exs
+++ config/config.exs
@@ -39,9 +39,9 @@ config :esbuild,
   version: "0.25.5",
   secop_service: [
     args:
-      ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets --external:/fonts/* --external:/images/* --alias:@=.),
+      ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=.),
     cd: Path.expand("../assets", __DIR__),
-    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+    env: %{"NODE_PATH" => [Path.expand("../deps", __DIR__), Mix.Project.build_path()]}
   ]
 
 # Configure tailwind (the version is required)
